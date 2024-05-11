d2f797fb296ca56d158e5542ada062ff8ce17ff0
SwarmYMD <kyamada0for0isl@gmail.com>
2024-05-11 21:35:23 +0900

    定数の設定

---
+++ work/Constants.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Constants{
+    public static final int M = 100;
+    public static final int N = 100;
+    public static final int m = 10;
+    public static final int n = 10;
+    public static final int W = M/m;
+    public static final int H = N/n;
+    
+    public static final int AGENT_NUM = 500;
+    
+    // paramaters of this system
+    public static final int T_max = 100;
+    public static final double alpha = 0.9;
+    public static final double c = 0.8;
+    public static final double Q = 1.0;
+    public static final double tau_max = 3000;
+    
+    // paramaters of agents
+    public static final int range = 3;
+    public static final double w = 0.6;
+    public static final double c_1 = 1.0;
+    public static final double c_2 = 1.0;
+    public static final int T = 10;
+    public static final double S_g = 0.9;
+
+    public static final int[] dir_row = {-1, 1, 0, 0};
+    public static final int[] dir_col = {0, 0, -1, 1};
+}
