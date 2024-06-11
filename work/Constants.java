5ddecbd9f2c0e73a2f728625f58ecda64810cc9e
SwarmYMD <kyamada0for0isl@gmail.com>
2024-06-12 04:12:07 +0900

    方向の情報を追加

--- work/Constants.java
+++ work/Constants.java
@@ -11,7 +11,7 @@ public class Constants{
     public static final int AGENT_NUM = 1000;
     
     // paramaters of this system
-    public static final int T_max = 100;
+    public static final int T_max = 50;
     public static final double alpha = 0.9;
     public static final double c = 0.8;
     public static final double Q = 1.0;
